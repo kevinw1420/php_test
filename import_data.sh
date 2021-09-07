f13776366d923e5d98574bf06a346a5c91fe1208
kevinw1420 <44896290+kevinw1420@users.noreply.github.com>
2021-09-08 00:48:25 +0800

    Update import_data.sh

--- import_data.sh
+++ import_data.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-docker exec php_test_db_1 /bin/bash /init_script/init.sh
+git clone git@github.com:datacharmer/test_db.git;
+cd ./test_db;
+cp -r . ../compose/mysql/test_data;
+docker exec php_test_db_1 /bin/bash /init_script/init.sh;
